0ebcfd426613108f3d2f01f663e872bbe44595ec
Michael Palmos <toqoz@hotmail.com>
2021-12-22 08:27:55 +1000

    Fix regression where format strings wouldn't work. #43

--- src/maths_utility.rs
+++ src/maths_utility.rs
@@ -443,12 +443,12 @@ pub fn format_action_notification_string(format_string: &str, action_name: &str,
 
                             continue
                     }
-                    _ => (),
                     "%s" => { formatted.extend_from_slice(notification.summary.as_bytes()); i += 2; continue },
                     "%b" => { formatted.extend_from_slice(notification.body.as_bytes()); i += 2; continue },
                     "%n" => { formatted.extend_from_slice(notification.app_name.as_bytes()); i += 2; continue },
                     "%a" => { formatted.extend_from_slice(action_name.as_bytes()); i += 2; continue },
                     "%i" => { formatted.extend_from_slice(notification.id.to_string().as_bytes()); i += 2; continue },
+                    _ => (),
                 }
 
                 formatted.push(b'%');
