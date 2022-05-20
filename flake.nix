84c1b920c89bd0ee3bc8d49626c438656d066ee3
Ash Walker <ashurstwalker@gmail.com>
2022-05-20 16:58:32 -0400

    Add System Files to Flake Output
    * Write `wired.service` to `$out/usr/lib/systemd/system/`
    * Write `wired{,_multilayout}.ron` to `$out/etc/wired/`

--- flake.nix
+++ flake.nix
@@ -39,7 +39,6 @@
               pname = "wired";
               src = ./.;
               meta = {
-                description = self.description;
                 homepage = "https://github.com/Toqozz/wired-notify";
                 downloadPage = "https://github.com/Toqozz/wired-notify/releases";
                 license = std.licenses.mit;
@@ -60,8 +59,19 @@
               ];
               # Without this wired_derive build would fail
               singleStep = true;
+              # install extra files (i.e. the systemd service)
+              postInstall =''
+                # /usr/bin/wired doesn't exist, here, because the binary will be somewhere in /nix/store,
+                # so this fixes the bin path in the systemd service and writes the updated file to the output dir.
+                mkdir -p $out/usr/lib/systemd/system
+                substitute ./wired.service $out/usr/lib/systemd/system/wired.service --replace /usr/bin/wired $out/bin/wired
+                # install example/default config files to etc/wired -- Arch packages seem to use etc/{pkg} for this,
+                # so there's precedent
+                install -Dm444 -t $out/etc/wired wired.ron wired_multilayout.ron
+              '';
             };
           };
+
           # `nix build`
           packages.wired = pkgs.wired;
           packages.default = self.packages.${system}.wired;
