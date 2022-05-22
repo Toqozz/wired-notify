0130769f52d277b5fbeba929a23867605b52e6be
Ash Walker <ashurstwalker@gmail.com>
2022-05-22 12:34:45 -0400

    Make services.wired.config nullable

--- home-manager.nix
+++ home-manager.nix
@@ -16,20 +16,25 @@ in {
       description = "Package providing wired.";
     };
     config = mkOption {
-      type = types.path;
-      default = ./wired.ron;
+      type = types.nullOr types.path;
+      default = null;
+      example = ./wired.ron;
       description = "File containing wired configuration.";
     };
   };
-  config = lib.mkIf cfg.enable {
-    home.packages = [cfg.package];
-    # Ideally, we could generate the config from a Nix expression,
-    # but that's complicated, so right now this just symlinks a file.
-    xdg.configFile."wired/wired.ron".source = cfg.config;
-    # As far as I know, the only "official" way to install systemd units
-    # through home-manager is to define `systemd.user.<unit name>`,
-    # which only allows unit configuration directly from Nix (i.e. you
-    # can't just give it a raw file). So, this just installs the existing service.
-    xdg.dataFile."systemd/user/wired.service".source = "${cfg.package}/usr/lib/systemd/system/wired.service";
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      home.packages = [cfg.package];
+      # As far as I know, the only "official" way to install systemd units
+      # through home-manager is to define `systemd.user.<unit name>`,
+      # which only allows unit configuration directly from Nix (i.e. you
+      # can't just give it a raw file). So, this just installs the existing service.
+      xdg.dataFile."systemd/user/wired.service".source = "${cfg.package}/usr/lib/systemd/system/wired.service";
+    })
+    (lib.mkIf (cfg.enable && cfg.config != null) {
+      # Ideally, we could generate the config from a Nix expression,
+      # but that's complicated, so right now this just symlinks a file.
+      xdg.configFile."wired/wired.ron".source = cfg.config;
+    })
+  ];
 }
