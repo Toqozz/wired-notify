86a35880caa2d2c3a40820ef5632e166dff54c90
TornaxO7 <tornax@proton.me>
2023-12-18 18:09:56 +0100

    replace flake-utils with nix-systemes
    makes the flake file cleaner

--- flake.nix
+++ flake.nix
@@ -3,81 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     alejandra = {
       url = "github:kamadorueda/alejandra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    alejandra,
-    ...
-  }: let
-    inherit (builtins) fromTOML readFile substring mapAttrs;
-
-    supportedSystems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-    ];
-    eachSupportedSystems = utils.lib.eachSystem supportedSystems;
-
-    cargoToml = fromTOML (readFile ./Cargo.toml);
-    version = "${cargoToml.package.version}_${substring 0 8 self.lastModifiedDate}_${self.shortRev or "dirty"}";
-
-    mkWired = {
-      lib,
-      rustPlatform,
-      dbus,
-      dlib,
-      cairo,
-      pango,
-      pkg-config,
-      xorg,
-      ...
+  outputs =
+    { self
+    , nixpkgs
+    , systems
+    , alejandra
+    , rust-overlay
+    , ...
     }:
-      rustPlatform.buildRustPackage {
-        name = "wired-${version}";
+    let
+      inherit (builtins) fromTOML readFile substring mapAttrs;
 
-        src = lib.cleanSource ./.;
-        cargoLock.lockFile = ./Cargo.lock;
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
 
-        # Requires dbus cairo and pango
-        # pkgconfig, glib and xorg are required for x11-crate
-        nativeBuildInputs = [pkg-config];
-        buildInputs = [
-          dbus
-          dlib
-          cairo
-          pango
-          xorg.libX11
-          xorg.libXi
-          xorg.libXrandr
-          xorg.libXcursor
-          xorg.libXScrnSaver
-        ];
-        # install extra files (i.e. the systemd service)
-        postInstall = ''
-          # /usr/bin/wired doesn't exist, here, because the binary will be somewhere in /nix/store,
-          # so this fixes the bin path in the systemd service and writes the updated file to the output dir.
-          mkdir -p $out/usr/lib/systemd/system
-          substitute ./wired.service $out/usr/lib/systemd/system/wired.service --replace /usr/bin/wired $out/bin/wired
-          # install example/default config files to etc/wired -- Arch packages seem to use etc/{pkg} for this,
-          # so there's precedent
-          install -Dm444 -t $out/etc/wired wired.ron wired_multilayout.ron
-        '';
+      cargoToml = fromTOML (readFile ./Cargo.toml);
+      version = "${cargoToml.package.version}_${substring 0 8 self.lastModifiedDate}_${self.shortRev or "dirty"}";
 
-        meta = {
-          homepage = "https://github.com/Toqozz/wired-notify";
-          downloadPage = "https://github.com/Toqozz/wired-notify/releases";
-          license = lib.licenses.mit;
+      mkWired =
+        { lib
+        , rustPlatform
+        , dbus
+        , dlib
+        , cairo
+        , pango
+        , pkg-config
+        , xorg
+        , ...
+        }:
+        rustPlatform.buildRustPackage {
+          name = "wired-${version}";
+
+          src = lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          # Requires dbus cairo and pango
+          # pkgconfig, glib and xorg are required for x11-crate
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [
+            dbus
+            dlib
+            cairo
+            pango
+            xorg.libX11
+            xorg.libXi
+            xorg.libXrandr
+            xorg.libXcursor
+            xorg.libXScrnSaver
+          ];
+          # install extra files (i.e. the systemd service)
+          postInstall = ''
+            # /usr/bin/wired doesn't exist, here, because the binary will be somewhere in /nix/store,
+            # so this fixes the bin path in the systemd service and writes the updated file to the output dir.
+            mkdir -p $out/usr/lib/systemd/system
+            substitute ./wired.service $out/usr/lib/systemd/system/wired.service --replace /usr/bin/wired $out/bin/wired
+            # install example/default config files to etc/wired -- Arch packages seem to use etc/{pkg} for this,
+            # so there's precedent
+            install -Dm444 -t $out/etc/wired wired.ron wired_multilayout.ron
+          '';
+
+          meta = {
+            homepage = "https://github.com/Toqozz/wired-notify";
+            downloadPage = "https://github.com/Toqozz/wired-notify/releases";
+            license = lib.licenses.mit;
+          };
         };
-      };
-  in ({
+    in
+    {
       # `nix fmt` (added in Nix 2.8)
       # (generating this outside of `eachDefaultSystem` because alejandra's supported systems may not match ours)
       formatter = mapAttrs (system: pkgs: pkgs.default) alejandra.packages;
@@ -87,41 +86,46 @@
 
       overlays = {
         default = final: prev: {
-          wired = prev.callPackage mkWired {};
+          wired = prev.callPackage mkWired { };
         };
       };
-    }
-    // (
-      eachSupportedSystems (system: let
-        pkgs = import nixpkgs {inherit system;};
-        wired = pkgs.callPackage mkWired {};
-      in {
-        # `nix build`
-        packages = {
-          inherit wired;
-          default = wired;
-        };
-        # `defaultPackage` deprecated in Nix 2.7
-        defaultPackage = self.packages.${system}.default;
 
-        # `nix run`
-        apps.wired = {
+      packages = eachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.callPackage mkWired { };
+        });
+
+      apps = eachSystem (system: {
+        default = self.apps.${system}.wired;
+
+        wired = {
           type = "app";
-          program = "${wired}/bin/wired";
+          program = "${self.packages.${system}.default}/bin/wired";
         };
-        apps.default = self.apps.${system}.wired;
-        # `defaultApp` deprecated in Nix 2.7
-        defaultApp = self.apps.${system}.default;
+      });
 
-        # `nix develop`
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs =
-            wired.nativeBuildInputs
-            ++ (with pkgs; [rustc cargo gcc clippy rustfmt]);
-          inherit (wired) buildInputs;
-        };
-        # `devShell` deprecated in Nix 2.7
-        devShell = self.devShells.${system}.default;
-      })
-    ));
+      devShells = eachSystem
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
+            };
+
+            wired = pkgs.callPackage mkWired { };
+            rust-toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            };
+          in
+          {
+            default = pkgs.mkShell {
+              packages = wired.nativeBuildInputs ++ [ rust-toolchain ];
+            };
+          });
+    };
 }
