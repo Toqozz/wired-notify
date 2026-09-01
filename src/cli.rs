6359bf2a331d42f195108ffe3c2275c1dcc03c85
Léana <leana.jiang+git@icloud.com>
2026-09-01 09:18:34 +0200

    make clippy happier

--- src/cli.rs
+++ src/cli.rs
@@ -382,7 +382,8 @@ pub fn process_cli(args: Vec<String>) -> Result<ShouldRun, String> {
                     let mut reader = BufReader::new(&sock);
                     let mut buf = String::with_capacity(8);
                     reader.read_line(&mut buf)?;
-                    Ok(println!("dnd: {}", buf))
+                    println!("dnd: {}", buf);
+                    Ok(())
                 })
                 .map_err(|e| e.to_string())?;
         }
